7381651443fe2b5cfb3f77aa39bb9dd6ae3b2182
Eren Halici <eren@halici.com.tr>
2013-03-26 18:08:33 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Brainquire::Application.config.secret_token = '2ff43167076e094146ab2014af51967ca4571e62514dc8e34cd3bdede71be643ed1ae5fe004d639b9a511409c44065ced55adddb6040c23a717d2df0ae9c4541'
